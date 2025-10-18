f0e82e56f45e2210486c7a85a71366266aa6b752
samumoil <samumoil@peikkoposti.com>
2025-10-18 14:19:30 +0300

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import GetVMs from './GetVMs'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- frontend/src/GetVMs.jsx
+++ frontend/src/GetVMs.jsx
@@ -3,18 +3,24 @@ import VMCards from "./VMCards";
 
 function GetVMs() {
   const [VMs, setVMs] = useState([]); // Tilamuuttuja VM-datalle
-  const API_URL = import.meta.env.VITE_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL; // Haetaan API-muuttuja .env tiedostosta
 
   const getData = async () => {
-    const response = await fetch(`${API_URL}/vms`);
+    const response = await fetch(`${API_URL}/vms`); // 
     console.log(response);
     const data = await response.json();
     setVMs(data);
   }
 
   useEffect(() => {
-    getData();
-  }, []); //
+    getData(); // Haetaan heti komponentin latautuessa
+
+    const interval = setInterval(() => {
+      getData(); // Haetaan 10 sekunnin välein
+    }, 10000);
+
+    return () => clearInterval(interval); // Siivotaan intervalli
+  }, []);
 
 
   return (
